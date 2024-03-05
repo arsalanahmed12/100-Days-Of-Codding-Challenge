18d6cd1c8766a4a736d752596e693e7e34c94722
arsalanahmed12 <khanarsal884@gmail.com>
2024-03-06 00:04:25 +0500

    second commit

---
+++ day1.js
@@ -0,0 +1,2 @@
+var name1 = "Arsalan Ahmed";
+console.log("Hello ".concat(name1, " would you like to learn some TypeScript today?"));
---
+++ day1.ts
@@ -0,0 +1,2 @@
+let name1: string="Arsalan Ahmed"
+console.log(`Hello ${name1} would you like to learn some TypeScript today?`)
