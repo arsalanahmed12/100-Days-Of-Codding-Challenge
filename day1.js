a61c037e523e14587cb8832071ef348af0b8357b
arsalanahmed12 <khanarsal884@gmail.com>
2024-03-06 00:30:02 +0500

    day1 compelete

--- day1.js
+++ day1.js
@@ -1,2 +1,9 @@
+// Question 1: Make enviroment in your laptop.
+// Question 2: Store a persons`s name in a variable and print a message to them.
 var name1 = "Arsalan Ahmed";
 console.log("Hello ".concat(name1, " would you like to learn some TypeScript today?"));
+// Question 3: Store a person`s name in a variable, and then print that persons`s name in lowercase, uppercase, and titlecase.
+var name2 = "Naveed";
+console.log(name2.toLowerCase());
+console.log(name2.toUpperCase());
+console.log(name2.charAt(0).toUpperCase() + name2.slice(1 - 6).toLowerCase());
--- day1.ts
+++ day1.ts
@@ -1,2 +1,15 @@
+// Question 1: Make enviroment in your laptop.
+// Question 2: Store a persons`s name in a variable and print a message to them.
+
+
 let name1: string="Arsalan Ahmed"
 console.log(`Hello ${name1} would you like to learn some TypeScript today?`)
+
+// Question 3: Store a person`s name in a variable, and then print that persons`s name in lowercase, uppercase, and titlecase.
+
+let name2: string="Naveed"
+console.log(name2.toLowerCase())
+
+console.log(name2.toUpperCase())
+
+console.log(name2.charAt(0).toUpperCase()+name2.slice(1-6).toLowerCase())
