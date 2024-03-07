5b3ac71af54ab0357c585fb92d7455807a54f4e3
arsalanahmed12 <khanarsal884@gmail.com>
2024-03-07 20:40:33 +0500

    Day3 challenge compelete.

---
+++ day3.js
@@ -0,0 +1,14 @@
+// Question 7: Write addition, subtraction, multipliction, and division operations that each result in the number 8 . Enclose your operations in print statements.
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
+// Question 8: You should creat four lines that look like this: console.log(5+3) your output should simply be four lines wut the number 8 appearing once on each line.
+console.log(5 + 3);
+console.log(5 + 3);
+console.log(5 + 3);
+console.log(5 + 3);
+// Question 9: Store your favorite number in a variable. Then creat a message revealing your favorite number. Print that message.
+var favorite_number = 4;
+var message = "My favorite number is ".concat(favorite_number);
+console.log(message);
